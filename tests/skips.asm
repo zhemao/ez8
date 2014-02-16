199d01c20f2d4074ad892c2cdf7fa2ff0f2fae4f
Howard Mao <zhehao.mao@gmail.com>
2014-02-16 12:40:33 -0800

    test skip instruction

---
+++ tests/skips.asm
@@ -0,0 +1,27 @@
+.org 0
+goto main
+
+.org 5
+main
+  set 0
+  skeqz
+  goto error
+  set 1
+  sknez
+  goto error
+  skgtz
+  goto error
+  skgez
+  goto error
+  set -1
+  skltz
+  goto error
+  put 0x21
+  sklez 0x21
+  goto error
+  set 0
+  ret
+
+error
+  set 1
+  ret
