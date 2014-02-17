a6d69578043378e300547852f25a1e8df37b2b1a
Howie Mao <zhehao.mao@gmail.com>
2014-02-17 10:02:50 -0800

    add instruction numbering to test programs

--- tests/arithmetic.asm
+++ tests/arithmetic.asm
@@ -6,61 +6,61 @@ goto main
 .org 5
 main:
 ; test addition and subtractioin
-  set 25
-  put 0x21
-  set 5
-  add 0x21
-  subl 30
-  skeqz
-  goto error
-  set 0xff
-  addl 1
-  skbs STATUS 1
-  goto error
-  set 27
-  adcl 2
-  subl 29
-  skbc STATUS 0
-  goto error
+  set 25        ; 5
+  put 0x21      ; 6
+  set 5         ; 7
+  add 0x21      ; 8
+  subl 30       ; 9
+  skeqz         ; 10
+  goto error    ; 11
+  set 0xff      ; 12
+  addl 1        ; 13
+  skbs STATUS 1 ; 14
+  goto error    ; 15
+  set 27        ; 16
+  adcl 2        ; 17
+  subl 29       ; 18
+  skbc STATUS 0 ; 19
+  goto error    ; 20
 ; test shifting
-  set 0x80
-  sral 3
-  subl 0xf0
-  skbs STATUS 0
-  goto error
-  set 0x80
-  srll 3
-  slll 1
-  put 0x21
-  subl 0x20
-  skbs STATUS 0
-  goto error
+  set 0x80      ; 21
+  sral 3        ; 22
+  subl 0xf0     ; 23
+  skbs STATUS 0 ; 24
+  goto error    ; 25
+  set 0x80      ; 26
+  srll 3        ; 27
+  slll 1        ; 28
+  put 0x21      ; 29
+  subl 0x20     ; 30
+  skbs STATUS 0 ; 31
+  goto error    ; 32
 ; test bitwise operations
-  set 0x50
-  ior 0x21 M
-  set 0x10
-  xor 0x21 M
-  set 0x0f
-  ior 0x21 M
-  set 0xfc
-  and 0x21
-  subl 0x6c
-  skbs STATUS 0
-  goto error
+  set 0x50      ; 33
+  ior 0x21 M    ; 34
+  set 0x10      ; 35
+  xor 0x21 M    ; 36
+  set 0x0f      ; 37
+  ior 0x21 M    ; 38
+  set 0xfc      ; 39
+  and 0x21      ; 40
+  subl 0x6c     ; 41
+  skbs STATUS 0 ; 42
+  goto error    ; 43
 ; test clr and com
-  clr
-  skeqz
-  goto error
-  set 0xa3
-  put 0x21
-  com 0x21
-  get 0x21
-  subl 0x5c
-  skbs STATUS 0
-  goto error
-  set 0
-  ret
+  clr           ; 44
+  skeqz         ; 45
+  goto error    ; 46
+  set 0xa3      ; 47
+  put 0x21      ; 48
+  com 0x21      ; 49
+  get 0x21      ; 50
+  subl 0x5c     ; 51
+  skbs STATUS 0 ; 52
+  goto error    ; 53
+  set 0         ; 54
+  ret           ; 55
 
 error:
-  set 1
-  ret
+  set 1         ; 56
+  ret           ; 57
--- tests/skips.asm
+++ tests/skips.asm
@@ -3,25 +3,25 @@ goto main
 
 .org 5
 main:
-  set 0
-  skeqz
-  goto error
-  set 1
-  sknez
-  goto error
-  skgtz
-  goto error
-  skgez
-  goto error
-  set -1
-  skltz
-  goto error
-  put 0x21
-  sklez 0x21
-  goto error
-  set 0
-  ret
+  set 0      ; 5
+  skeqz      ; 6
+  goto error ; 7
+  set 1      ; 8
+  sknez      ; 9
+  goto error ; 10
+  skgtz      ; 11
+  goto error ; 12
+  skgez      ; 13
+  goto error ; 14
+  set -1     ; 15
+  skltz      ; 16
+  goto error ; 17
+  put 0x21   ; 18
+  sklez 0x21 ; 19
+  goto error ; 20
+  set 0      ; 21
+  ret        ; 22
 
 error:
-  set 1
-  ret
+  set 1      ; 23
+  ret        ; 24
