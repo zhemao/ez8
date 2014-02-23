241ae3b50c55d74989922d9b6a446b75ab7185b7
Howard Mao <zhehao.mao@gmail.com>
2014-02-23 10:36:27 -0800

    track some more signals in testbench

--- processor/tests/ez8_cpu_sim_setup.tcl
+++ processor/tests/ez8_cpu_sim_setup.tcl
@@ -7,13 +7,15 @@ add wave {error}
 add wave -radix unsigned instr_writeaddr
 add wave -radix hexadecimal instr_writedata
 
-add wave -radix hexadecimal cpu/pc
-add wave -radix hexadecimal accum
+add wave -radix unsigned cpu/pc
+add wave -radix decimal accum
 add wave -radix hexadecimal cpu/instr
+add wave -radix hexadecimal cpu/opcode
 add wave cpu/pc_kill
 add wave cpu/kill_write
 add wave cpu/goto
 add wave cpu/ret
 add wave cpu/call
+add wave cpu/skip
 
 run 3000 ns
