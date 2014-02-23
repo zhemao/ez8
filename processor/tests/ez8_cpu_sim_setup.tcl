a4309749aad94772b918bfeff2e023d459aa2460
Howard Mao <zhehao.mao@gmail.com>
2014-02-22 18:23:27 -0800

    attempting to testbench arithmetic.asm

--- processor/tests/ez8_cpu_sim_setup.tcl
+++ processor/tests/ez8_cpu_sim_setup.tcl
@@ -1,6 +1,8 @@
 add wave clk
 add wave reset
 add wave pause
+add wave stopped
+add wave {error}
 
 add wave -radix hexadecimal cpu/pc
 add wave -radix hexadecimal accum
@@ -8,5 +10,7 @@ add wave -radix hexadecimal cpu/instr
 add wave cpu/pc_kill
 add wave cpu/kill_write
 add wave cpu/goto
+add wave cpu/ret
+add wave cpu/call
 
-run 600 ns
+run 1200 ns
--- processor/tests/ez8_cpu_tb.sv
+++ processor/tests/ez8_cpu_tb.sv
@@ -8,11 +8,17 @@ reg [11:0] instr_writeaddr;
 reg [15:0] instr_writedata;
 reg instr_write_en;
 wire [7:0] accum;
+wire error;
+wire stopped;
 
 ez8_cpu cpu (
     .clk (clk),
     .reset (reset),
     .pause (pause),
+
+    .error (error),
+    .stopped (stopped),
+
     .instr_writeaddr (instr_writeaddr),
     .instr_writedata (instr_writedata),
     .instr_write_en (instr_write_en),
@@ -22,32 +28,15 @@ ez8_cpu cpu (
 always #10000 clk = !clk;
 
 initial begin
-    reset = 1'b0;
-    pause = 1'b1;
-    instr_writeaddr = 12'd0;
-    instr_writedata = 16'h4050;
-    instr_write_en = 1'b1;
-
-    #20000 instr_writeaddr = 12'd1;
-    instr_writedata = 16'h0101;
-    #20000 instr_writeaddr = 12'd2;
-    instr_writedata = 16'h4100;
-    #20000 instr_writeaddr = 12'd3;
-    instr_writedata = 16'h2100;
-    #20000 instr_writeaddr = 12'd4;
-    instr_writedata = 16'h6148;
-    #20000 instr_writeaddr = 12'd5;
-    instr_writedata = 16'h800a;
-    #20000 instr_writeaddr = 12'd6;
-    instr_writedata = 16'h6050;
-    #20000 instr_writeaddr = 12'ha;
-    instr_writedata = 16'h6060;
-    #20000 instr_write_en = 1'b0;
-    pause = 1'b0;
     reset = 1'b1;
+    pause = 1'b0;
+    instr_write_en = 1'b0;
     #20000 reset = 1'b0;
-    #200000 pause = 1'b1;
-    #20000 assert (accum == 8'd7);
+
+    #1160000 assert (stopped);
+
+    assert (!error);
+    assert (accum == 8'd0);
 end
 
 endmodule
